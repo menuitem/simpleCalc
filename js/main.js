cfa363981e114021f70b50b2b64609a3095df214
lucjan <lucjan.stepien@student.ncirl.ie>
2014-03-26 21:05:40 +0000

    some styles

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 window._skel_config = {
-	prefix: 'css/style',
-	preloadStyleSheets: true,
-	resetCSS: true,
-	boxModel: 'border',
-	grid: { gutters: 30 },
-	breakpoints: {
-		wide: { range: '1200-', containers: 1140, grid: { gutters: 50 } },
-		narrow: { range: '481-1199', containers: 960 },
-		narrower: { range: '481-960', containers: 500 },
-		mobile: { range: '-480', containers: 'fluid', lockViewport: true, grid: { collapse: true, gutters: 20 } }
-	}
+    prefix: 'css/style',
+        preloadStyleSheets: true,
+    resetCSS: true,
+    boxModel: 'border',
+    grid: { gutters: 30 },
+    breakpoints: {
+        wide: { range: '1200-', containers: 1140, grid: { gutters: 50 } },
+        narrow: { range: '481-1199', containers: 960 },
+        mobile: { range: '-480', containers: 'fluid', lockViewport: true, grid: { collapse: true } }
+    }
 };
+
 document.addEventListener("DOMContentLoaded", function(event) {
 	input1.addEventListener('click', function(){
 		score.value=null;
